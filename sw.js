8e50e3e56108e64a619a4bec59f8fb687e838981
karthisethunathan-lab <karthisethunathan@gmail.com>
2026-04-14 12:11:42 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,19 @@
+const CACHE_NAME = 'sri-mangalya-v1';
+const assets = [
+  './',
+  './index.html',
+  './manifest.json',
+  './logo.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(assets))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
